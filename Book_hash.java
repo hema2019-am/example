7d9ed92d0a1f3e19de639272ae3add6c54973c6d
hema2019 <17061999hema2019-am@users.noreply.github.com>
2019-12-22 15:57:48 +0530

    a new file

---
+++ Book_hash.java
@@ -0,0 +1,15 @@
+
+public class Book_hash {
+	String title;
+	String author;
+	int numberOfPages;
+	int publishedYear;
+	int edition;
+	String ISBN;
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
